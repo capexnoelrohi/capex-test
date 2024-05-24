72f406fe874288c9a31b84d9eec953a0202c494e
Noel Rohi <nrcgarcia1558@gmail.com>
2024-05-24 16:42:39 +0800

    fix: schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { env } from "@/env.mjs";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "src/db/schema/fims/index.ts",
+  schema: "src/db/schema/index.ts",
   dialect: "mysql",
   out: "migrations",
   dbCredentials: {
